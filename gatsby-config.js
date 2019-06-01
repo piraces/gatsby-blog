5d1822c846182eaeafe155c1d87871a5efab5215
Raúl Piracés Alastuey <piraces@tanque.local>
2019-06-01 19:31:25 +0200

    Fix Config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ const postCssPlugins = require('./postcss-config.js');
 module.exports = {
   siteMetadata: {
     url: siteConfig.url,
+    siteUrl: siteConfig.url,
     title: siteConfig.title,
     subtitle: siteConfig.subtitle,
     copyright: siteConfig.copyright,
