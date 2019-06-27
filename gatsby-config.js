e52a08e8c4342f543f2682325a3f44cb6535b769
piraces <raul.piraces@gmail.com>
2019-06-27 19:11:13 +0200

    Attempt to fix public path for netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,6 +121,7 @@ module.exports = {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
+        publicPath: 'admin/index.html'
       }
     },
     {
