1de512e988d3d08977bfb5857988e04bdd80a133
piraces <raul.piraces@gmail.com>
2020-01-22 20:20:41 +0100

    Define headers in gatsby-plugin-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,7 +118,35 @@ module.exports = {
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
-    'gatsby-plugin-netlify',
+    {
+      resolve: 'gatsby-plugin-netlify',
+      options: {
+        allPageHeaders: [
+          "X-Frame-Options: DENY",
+          "X-XSS-Protection: 1; mode=block",
+          "X-Content-Type-Options: nosniff",
+          "Strict-Transport-Security: max-age=63072000; includeSubDomains; preload",
+          "Content-Security-Policy: default-src 'none'; script-src 'self' www.google-analytics.com ajax.googleapis.com www.googletagmanager.com; connect-src 'self' identity.services.netlify.com github.com; img-src 'self'; style-src 'self';",
+          "Expect-CT: max-age=604800",
+          "Expect-CT: enforce",
+          "Feature-Policy: autoplay 'none'; camera 'none'",
+          "Referrer-Policy: no-referrer-when-downgrade",
+        ],
+        headers: {
+          "/*": [
+            "X-Frame-Options: DENY",
+            "X-XSS-Protection: 1; mode=block",
+            "X-Content-Type-Options: nosniff",
+            "Strict-Transport-Security: max-age=63072000; includeSubDomains; preload",
+            "Content-Security-Policy: default-src 'none'; script-src 'self' www.google-analytics.com ajax.googleapis.com www.googletagmanager.com; connect-src 'self' identity.services.netlify.com github.com; img-src 'self'; style-src 'self';",
+            "Expect-CT: max-age=604800",
+            "Expect-CT: enforce",
+            "Feature-Policy: autoplay 'none'; camera 'none'",
+            "Referrer-Policy: no-referrer-when-downgrade",
+          ]
+        }
+      }
+    },
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
